e2ab870544681cae83738adf7c4199109a19e6eb
Karthick508 <karthivkn13@gmail.com>
2024-03-24 09:38:44 +0530

    hello world spring

---
+++ src/main/java/com/cicd/cicdMs/CicdMsApplication.java
@@ -0,0 +1,14 @@
+package com.cicd.cicdMs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdMsApplication.class, args);
+		System.out.println("Hello world spring boot");
+	}
+
+}
---
+++ src/test/java/com/cicd/cicdMs/CicdMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.cicdMs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
